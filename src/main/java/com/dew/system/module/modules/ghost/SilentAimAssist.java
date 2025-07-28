4974f209d990597d6380952681dc95cddb3eaf17
Aspw <75427057+Aspw-w@users.noreply.github.com>
2025-07-28 22:51:11 +0900

    improved lag based things

--- src/main/java/com/dew/system/module/modules/ghost/SilentAimAssist.java
+++ src/main/java/com/dew/system/module/modules/ghost/SilentAimAssist.java
@@ -4,8 +4,11 @@ import com.dew.DewCommon;
 import com.dew.system.event.events.TickEvent;
 import com.dew.system.module.Module;
 import com.dew.system.module.ModuleCategory;
+import com.dew.system.module.modules.combat.Teams;
+import com.dew.system.module.modules.player.Freecam;
 import com.dew.system.settingsvalue.NumberValue;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.MovingObjectPosition;
 import org.lwjgl.input.Keyboard;
@@ -16,25 +19,27 @@ public class SilentAimAssist extends Module {
         super("Silent Aim Assist", ModuleCategory.GHOST, Keyboard.KEY_NONE, false, true, true);
     }
 
-    private static final NumberValue triggerAngleDifference = new NumberValue("Trigger Angle Difference", 35.0, 20.0, 90.0, 0.5);
+    private static final NumberValue activateAngleDifference = new NumberValue("Activate Angle Difference", 25.0, 20.0, 90.0, 0.5);
 
     @Override
     public void onTick(TickEvent event) {
         if (mc.thePlayer == null || mc.theWorld == null || mc.objectMouseOver == null || mc.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.ENTITY) return;
 
         Entity closest = null;
-        float minAngleDiff = triggerAngleDifference.get().floatValue();
+        float minAngleDiff = activateAngleDifference.get().floatValue();
 
         for (Entity entity : mc.theWorld.loadedEntityList) {
             if (entity == null || entity == mc.thePlayer || entity.isDead || !(entity instanceof EntityPlayer)) continue;
             if (!entity.canBeCollidedWith() || !entity.isEntityAlive()) continue;
             if (!mc.thePlayer.canEntityBeSeen(entity) || mc.thePlayer.getDistanceToEntity(entity) > 5f) continue;
+            if (DewCommon.moduleManager.getModule(Teams.class).isInYourTeam((EntityLivingBase) entity)) continue;
+            if (DewCommon.moduleManager.getModule(Freecam.class).isEnabled() && mc.thePlayer == entity) continue;
 
             float[] diffs = DewCommon.rotationManager.getAngleDifferenceTo(entity);
             float yawDiff = diffs[0];
             float pitchDiff = diffs[1];
 
-            if (yawDiff <= triggerAngleDifference.get() && pitchDiff <= triggerAngleDifference.get()) {
+            if (yawDiff <= activateAngleDifference.get() && pitchDiff <= activateAngleDifference.get()) {
                 float totalDiff = yawDiff + pitchDiff;
                 if (totalDiff < minAngleDiff) {
                     minAngleDiff = totalDiff;
