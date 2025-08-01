6d4dbb97e9a76da9d4f45f18991f505ff3ec93f9
Aspw <75427057+Aspw-w@users.noreply.github.com>
2025-08-01 19:23:21 +0900

    ABBB

--- src/main/java/com/dew/system/module/ModuleManager.java
+++ src/main/java/com/dew/system/module/ModuleManager.java
@@ -35,7 +35,7 @@ public class ModuleManager implements EventListener {
                 new Ambience(), new NoJumpDelay(), new SafetySwitchv2000(), new AntiExploit(),
                 new FastPlace(), new BridgeAssist(), new Clicker(), new FakeLag(),
                 new SilentAimAssist(), new Freecam(), new Trajectories(), new PacketBlaster(),
-                new ColorBlindnessAssistant()
+                new ColorBlindnessAssistant(), new Scaffold2()
         ));
 
         DewCommon.eventManager.register(this);
