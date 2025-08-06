43827720b9006d219737fa77138b2d733f323dc0
Aspw <75427057+Aspw-w@users.noreply.github.com>
2025-08-06 23:34:02 +0900

    Update MicrosoftAuth.java

--- src/main/java/com/dew/system/altmanager/login/MicrosoftAuth.java
+++ src/main/java/com/dew/system/altmanager/login/MicrosoftAuth.java
@@ -64,7 +64,7 @@ public class MicrosoftAuth {
             LogUtil.infoLog("4");
 
             MCAuthToken mcAuthToken = getMCAuthToken(xblToken, xstsToken);
-            DewCommon.altManager.status = "Got MCAuth token";
+            DewCommon.altManager.status = "Got MCAuth token (Copied to clipboard lol best feature 2025)";
 
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             StringSelection stringSelection = new StringSelection(mcAuthToken.accessToken);
