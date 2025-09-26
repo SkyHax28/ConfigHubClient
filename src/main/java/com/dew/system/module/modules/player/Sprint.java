81f7763e2722658d1d3210d1291f93129f96e63b
Aspw <75427057+Aspw-w@users.noreply.github.com>
2025-09-27 00:52:37 +0900

    Update Sprint.java

--- src/main/java/com/dew/system/module/modules/player/Sprint.java
+++ src/main/java/com/dew/system/module/modules/player/Sprint.java
@@ -15,6 +15,7 @@ public class Sprint extends Module {
 
     private static final BooleanValue omni = new BooleanValue("Omni", true);
     private static final BooleanValue rotationsCheck = new BooleanValue("Rotations Check", false);
+    private static final BooleanValue noCheckWhenScaffold = new BooleanValue("No Check When Scaffold", false, rotationsCheck::get);
 
     public Sprint() {
         super("Sprint", ModuleCategory.PLAYER, Keyboard.KEY_NONE, false, true, true);
@@ -26,7 +27,7 @@ public class Sprint extends Module {
 
     public boolean shouldNotSprint() {
         float yawDiff = Math.abs(MovementUtil.getAngleDifference((float) MovementUtil.getDirection(), DewCommon.rotationManager.getClientYaw()));
-        return !MovementUtil.isMoving() || mc.thePlayer.isSneaking() || mc.thePlayer.getFoodStats().getFoodLevel() <= 6 && mc.playerController != null && (mc.playerController.getCurrentGameType() == WorldSettings.GameType.SURVIVAL || mc.playerController.getCurrentGameType() == WorldSettings.GameType.ADVENTURE) || mc.thePlayer.isCollidedHorizontally || this.isEnabled() && !omni.get() && !mc.gameSettings.keyBindForward.isKeyDown() || (!this.isEnabled() || !omni.get()) && DewCommon.rotationManager.isRotating() && DewCommon.moduleManager.getModule(MoveFix.class).isEnabled() && yawDiff > 30F || mc.thePlayer.isUsingItem() && !DewCommon.moduleManager.getModule(NoSlow.class).canNoSlow() || DewCommon.moduleManager.getModule(Scaffold.class).isEnabled() && DewCommon.moduleManager.getModule(Scaffold.class).isNoSprint() || DewCommon.rotationManager.isRotating() && rotationsCheck.get() && yawDiff > 30F;
+        return !MovementUtil.isMoving() || mc.thePlayer.isSneaking() || mc.thePlayer.getFoodStats().getFoodLevel() <= 6 && mc.playerController != null && (mc.playerController.getCurrentGameType() == WorldSettings.GameType.SURVIVAL || mc.playerController.getCurrentGameType() == WorldSettings.GameType.ADVENTURE) || mc.thePlayer.isCollidedHorizontally || this.isEnabled() && !omni.get() && !mc.gameSettings.keyBindForward.isKeyDown() || (!this.isEnabled() || !omni.get()) && DewCommon.rotationManager.isRotating() && DewCommon.moduleManager.getModule(MoveFix.class).isEnabled() && yawDiff > 30F || mc.thePlayer.isUsingItem() && !DewCommon.moduleManager.getModule(NoSlow.class).canNoSlow() || DewCommon.moduleManager.getModule(Scaffold.class).isEnabled() && DewCommon.moduleManager.getModule(Scaffold.class).isNoSprint() || DewCommon.rotationManager.isRotating() && rotationsCheck.get() && yawDiff > 30F && (!noCheckWhenScaffold.get() || !DewCommon.moduleManager.getModule(Scaffold.class).isEnabled());
     }
 
     @Override
